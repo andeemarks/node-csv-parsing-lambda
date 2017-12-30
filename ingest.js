85730f453dfb3ed8b25edd58f4d4a5651cb7bfd5
Andy Marks <vampwillow@gmail.com>
2017-12-31 07:57:20 +1100

    Needed manual addition of S3 permissions to policy in AWS console, but manages to find S3 file

--- ingest.js
+++ ingest.js
@@ -8,25 +8,21 @@ module.exports.handler = (event, context, callback) => {
   var key = event.Records[0].s3.object.key;
   s3.getObject({Bucket: bucket, Key: key}, function(err, data) {
     if (err) {
-      console.log("Error getting object " + key + " from bucket " + bucket +
-          ". Make sure they exist and your bucket is in the same region as this function.");
-      context.fail ("Error getting file: " + err)      
+      context.fail ("Error getting file: (" + bucket + ", " + key + ") " + err);
+      return;      
     } else {
       console.log('CONTENT TYPE:', data.ContentType);
       context.succeed();
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Hello Andy!',
+          input: event,
+        }),
+      };
+
+      callback(null, response);
     }
   });
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello Andy!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
